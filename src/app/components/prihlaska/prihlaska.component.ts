665fd02f15916a1d007810002052bbc43d943ec4
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2022-03-09 15:52:41 +0100

    added referencia component

---
+++ src/app/components/prihlaska/prihlaska.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prihlaska',
+  templateUrl: './prihlaska.component.html',
+  styleUrls: ['./prihlaska.component.scss']
+})
+export class PrihlaskaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
