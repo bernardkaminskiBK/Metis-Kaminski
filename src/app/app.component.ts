d13f4eb37ad86e25a770a7cc46047cd2e1f09798
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2022-05-05 12:01:44 +0200

    Commented recursion calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,15 @@ export class AppComponent implements OnInit{
   title = 'Metis-Kaminski';
 
   ngOnInit(): void {
-    const stock = new Stock();
-
-    console.log('Celkova hodnota skladu: ' + stock.getStockValue2());
-
-    const productNames = stock.getAllProductName();
-    productNames.forEach((productName) => {
-      console.log('Product names: ' + productName);
-    });
+    // Rekurzivne volania priklad
+    // const stock = new Stock();
+    //
+    // console.log('Celkova hodnota skladu: ' + stock.getStockValue2());
+    //
+    // const productNames = stock.getAllProductName();
+    // productNames.forEach((productName) => {
+    //   console.log('Product names: ' + productName);
+    // });
   }
 
 
