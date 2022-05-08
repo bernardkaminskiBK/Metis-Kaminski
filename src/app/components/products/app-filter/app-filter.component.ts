eb3ba1b2a3a3be226b2879b2305ab61f8c3c62e7
bernardkaminskBK <bernardkaminski.bk@gmail.com>
2022-05-08 15:18:02 +0200

    Refactored app filter deleted unused parameter

--- src/app/components/products/app-filter/app-filter.component.ts
+++ src/app/components/products/app-filter/app-filter.component.ts
@@ -1,15 +1,25 @@
 import {
-  AfterContentChecked, AfterContentInit, AfterViewChecked,
-  Component, DoCheck, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges,
-  ViewChild
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  Component,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
 } from '@angular/core';
-import {ProductService} from "../../../shared/services/product.service";
-import {Product} from "../../../models/Product";
+import { ProductService } from '../../../shared/services/product.service';
+import { Product } from '../../../models/Product';
 
 @Component({
   selector: 'app-app-filter',
   templateUrl: './app-filter.component.html',
-  styleUrls: ['./app-filter.component.scss']
+  styleUrls: ['./app-filter.component.scss'],
 })
 export class AppFilterComponent implements OnChanges {
   searchInputText: string = '';
@@ -18,27 +28,31 @@ export class AppFilterComponent implements OnChanges {
 
   @Input('listOfProducts') data: Product[];
   @Input('isCheckBoxChecked') isCheckBoxChecked: boolean;
-  @Output('filteredList') filteredData: EventEmitter<Product[]> = new EventEmitter<Product[]>();
+  @Output('filteredList') filteredData: EventEmitter<Product[]> =
+    new EventEmitter<Product[]>();
 
-  constructor(private productService: ProductService) {
-  }
+  constructor(private productService: ProductService) {}
 
   private array: Product[];
   private subscription: any;
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.subscription = this.productService.productListObserver.subscribe((newValue) => {
-      this.array = newValue;
-    });
+  ngOnChanges() {
+    this.subscription = this.productService.productListObserver.subscribe(
+      (newValue) => {
+        this.array = newValue;
+      }
+    );
     this.filter();
   }
 
   filter(): void {
     if (this.array.length >= 2 || this.isCheckBoxChecked) {
       if (this.array && this.array.length) {
-        return this.filteredData.emit(this.array.filter((product: Product) => {
-          return this.filterByTitle(product) && this.filterByInStock(product);
-        }));
+        return this.filteredData.emit(
+          this.array.filter((product: Product) => {
+            return this.filterByTitle(product) && this.filterByInStock(product);
+          })
+        );
       }
       this.filteredData.emit([]);
     } else {
@@ -52,7 +66,9 @@ export class AppFilterComponent implements OnChanges {
 
   private filterByTitle(product: Product): boolean {
     if (this.searchInputText.length >= 2) {
-      return product.name.toLowerCase().includes(this.searchInputText.toLowerCase());
+      return product.name
+        .toLowerCase()
+        .includes(this.searchInputText.toLowerCase());
     }
     return true;
   }
@@ -72,5 +88,4 @@ export class AppFilterComponent implements OnChanges {
       this.subscription.unsubscribe();
     }
   }
-
 }
